6cae2920be0ee05717c0d6aac3c8cfe7d2a510a2
Administrator <Administrator@sumedha-PC>
2020-09-08 16:01:10 +0530

    Updated and Uploaded

--- modapp2/src/test/java/java9/mod/modapp2/MainTest.java
+++ modapp2/src/test/java/java9/mod/modapp2/MainTest.java
@@ -12,5 +12,16 @@ public class MainTest {
 		yakshaAssert(currentTest(),(Main.checkPrime(2)?"true":"false"),businessTestFile);
 		yakshaAssert(currentTest(),(Main.checkPrime(9)?"true":"false"),businessTestFile);
 	}
+	@Test
+    public void testExceptionConditon() throws Exception{
+	 yakshaAssert(currentTest(),true,boundaryTestFile);
+      }
+
+	@Test
+	public void testBoundaryCondition() throws Exception {
+	  yakshaAssert(currentTest(),true,exceptionTestFile);
+   }
+
+
 
 }
